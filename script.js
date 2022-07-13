5e06764a518763b6d7f0d0c6a101d7ad9445ed35
Yash <yashharale2003@gmail.com>
2022-07-13 13:03:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  //navigator.geolocation.getCurrentPosition(callback, error)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude]
+
+      const  map = L.map('map').setView(coords, 9);
+    //   console.log(map);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+        map.on('click', function(mapEvent) {
+            console.log(mapEvent);
+            const {lat, lng} = mapEvent.latlng
+
+            L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+                L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className:'running-popup',
+            })
+            )
+            .setPopupContent('Workout')
+            .openPopup();
+        })
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
