104763fb51202f6b62a03ab6fef1f79a14280359
Yash Harale <96224892+YashHarale@users.noreply.github.com>
2024-02-12 22:53:36 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -55,7 +55,20 @@ class Cycling extends Workout {
 }
 
 class Skipping extends Workout {
-  
+  type = 'skipping';
+
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+    this._setDescription();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
 }
 
 // ---------------------------------APPLICATION ARCHITECTURE---------------------------------
